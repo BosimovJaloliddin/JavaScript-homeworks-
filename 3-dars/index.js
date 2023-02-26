293d36b956140f2ba3d5059317cf7287b4904137
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-02-26 15:22:38 +0500

    if else, ternary operators,nullish

---
+++ 3-dars/index.js
@@ -0,0 +1,57 @@
+// 1
+// if ("0") {
+//   console.log("hellow"); //hello
+// }
+
+// ==========
+
+// 2
+// let nameJS = "ECMAScript";
+// if (nameJS == "ECMAScript") {
+//   console.log("Right");
+// } else {
+//   console.log("Your don't know");
+// }
+
+// nameJS == "ECMAScript" ? console.log("Right") : console.log("Your don't know");
+
+// =========
+// 3
+
+// let num = 0;
+// if (num > 0) {
+//   console.log(1);
+// } else if (num < 0) {
+//   console.log(-1);
+// } else {
+//   console.log(0);
+// }
+
+// ===========
+
+// 4
+// let result;
+// a + b < 4 ? (result = "Below") : (result = "Over");
+
+// // =============
+// // 5
+// let message;
+// logen == "Empolliye"
+//   ? (message = "Hellow")
+//   : logen == "Director"
+//   ? (message = "Greetings")
+//   : (logen = "" ? (message = "No logen") : (message = ""));
+
+// ===============
+// for (let i = 1; i <= 10; i++) {
+//   let str = 0;
+
+//   for (let j = 2; j < i; j++) {
+//     if (i % j == 0) {
+//       str = 1;
+//     }
+//   }
+//   if (i > 1 && str == 0) {
+//     console.log(i);
+//   }
+// }
