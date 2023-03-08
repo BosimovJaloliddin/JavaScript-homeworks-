a8432778f717dbd70c867ca981b7a9cbb57eafea
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-03-08 13:57:32 +0500

    object-2

---
+++ 7-dars/index.js
@@ -0,0 +1,31 @@
+// function makeUser() {
+//   return {
+//     name: "John",
+//     ref: this,
+//   };
+// }
+
+// let user = makeUser();
+
+// console.log(user.ref.name); // undefined
+
+// =====================
+
+let calculator = {
+  sum() {
+    return this.a + this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  },
+  read(a, b) {
+    this.a = a;
+    this.b = b;
+  },
+};
+
+calculator.read(1, 2);
+
+console.log(calculator.mul());
+console.log(calculator.sum());
