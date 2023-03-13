fd8059deac97f709891a68ae43a69ba270256ccc
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-03-13 12:24:53 +0500

    Object-2

--- 7-dars/index.js
+++ 7-dars/index.js
@@ -28,3 +28,34 @@
 // calculator.read(1, 2);
 // console.log(calculator.mul());
 // console.log(calculator.sum());
+
+// ============================
+
+// let ladder = {
+//   step: 0,
+//   up() {
+//     this.step++;
+//   },
+//   down() {
+//     this.step--;
+//   },
+//   showStep: function () {
+//     this.step = step;
+//   },
+// };
+
+// console.log(ladder.step);
+// ladder.up();
+// console.log(ladder.step);
+
+// ===========================
+// function A() {
+//   this.name = "Ali";
+// }
+// function B() {
+//   this.name = "Ali";
+// }
+// let a = new A();
+// let b = new B();
+
+// console.log(a == b); //false
