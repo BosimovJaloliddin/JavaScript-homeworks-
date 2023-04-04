b44f23dac50e63cf5517289277fb271f23dcf906
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-04-04 12:06:24 +0500

    lesson 17

---
+++ 17-dars/index.js
@@ -0,0 +1,10 @@
+// function chaqirilgandagi sonlar yig'indisini toping
+
+// const getSum = (n) => {
+//   return function (x) {
+//     if (x) return getSum(n + x);
+//     else return n;
+//   };
+// };
+
+// console.log(getSum(5)(4)(5)());
