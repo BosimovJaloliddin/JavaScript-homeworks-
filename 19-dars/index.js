78c947cb24ee7e6bfeddc9cfa9480df36b9b50ad
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-04-09 16:45:53 +0500

    19-dars

---
+++ 19-dars/index.js
@@ -0,0 +1,24 @@
+// applay,call va bind
+// let obj1 = {
+//   name: "Ali",
+//   age: 28,
+// };
+// let obj2 = {
+//   name: "Normat",
+//   age: 28,
+// };
+// let obj3 = {
+//   name: "Holid",
+//   age: 28,
+// };
+// function getInfo(one, two, there) {
+//   console.log(
+//     `Your name is ${this.name}, age ${this.age}. ${one} ${two}  ${there} `
+//   );
+// }
+
+// let info = getInfo.bind(obj1, "Welcome", "to", "telegram");
+// info();
+
+// getInfo.apply(obj2);
+// getInfo.apply(obj3);
