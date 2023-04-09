2e7f8209b5b7d10c101af117029c28871652d58b
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-04-09 22:14:17 +0500

    class advanced

---
+++ 25-dars/index.js
@@ -0,0 +1,64 @@
+// Classes Advanced
+// static
+
+// class User {
+//   static count = 0;
+//   static add() {
+//     console.log(++this.count);
+//   }
+// }
+
+// User.add();
+// User.add();
+// User.add();
+// User.add();
+// User.add();
+// User.add();
+
+// let us1 = new User();
+// let us2 = new User();
+
+// us1.add();
+// us1.add();
+// us1.add();
+// us1.add();
+// us1.add();
+// us1.add();
+// us2.add();
+// us1.add();
+
+// ==========================
+// class User {
+//   _name = "Ali";
+//   #age = 30;
+//   get info() {
+//     console.log(this.#age);
+//   }
+//   set info(vl) {
+//     this.#age = vl;
+//   }
+// }
+
+// let usr = new User();
+// usr.info = 90;
+// usr.info;
+
+// ======================
+// Built-in classes
+// class Ar extends Array {}
+// console.log(Ar.length);
+
+// instenceof
+// class Ar extends Array {}
+// let usr = new Ar();
+// let ar = [];
+// const info = () => {};
+// let str = "hi";
+
+// console.log(usr instanceof Object);
+// console.log(ar instanceof Array);
+// console.log(info instanceof Object);
+// console.log(info instanceof Array);
+
+// ========================
+// class Us0
