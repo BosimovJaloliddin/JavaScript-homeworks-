e67bfb190e275f86d2e13ca638728093ddb9b4f6
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-04-09 20:48:18 +0500

    getting and setting

---
+++ 22-dars/index.js
@@ -0,0 +1,31 @@
+// let obj = {
+//   name: "Ali",
+//   suename: "Aliyev",
+//   age: 25,
+// };
+
+// Object.defineProperty(obj, "name", {
+//   writable: false,
+//   configurable: false,
+//   enumerable: false,
+// });
+
+// for (i in obj) {
+//   console.log(i);
+// }
+
+// delete obj.age;
+// obj.age = 30;
+
+// console.log(Object.getOwnPropertyDescriptor(obj, "name"));
+
+// =====================================
+// setter and getter
+
+// let obj =0
+
+// obj.getting;
+
+// obj.getting = "hi gays";
+
+// console.log(obj.getting);
