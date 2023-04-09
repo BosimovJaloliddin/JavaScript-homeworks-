018ed54787c0d0f3cb13b9b712747d746a73f505
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-04-09 18:30:05 +0500

    prototype and __proto__

---
+++ 21-dars/index.js
@@ -0,0 +1,18 @@
+// prototype
+// let ar = [1, 2, 3, 4, 5];
+// Array.prototype.remove = function (n) {
+//   return this.filter((v) => v !== n);
+// };
+// console.log(ar.remove(1));
+
+// ========= __proto__
+
+// let obj1 = {
+//   name: "Ali",
+// };
+// let obj2 = {
+//   surname: "Aliyev",
+// };
+// obj1.__proto__ = obj2;
+
+// console.log(obj1.surname);
