c107ff2f8c9fdfa717aa86b0a16755efed14c2ba
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-04-10 05:20:29 +0500

    Module

---
+++ 28-dars/index.js
@@ -0,0 +1,29 @@
+// Module
+// let user = [
+//   { id: 1, name: "Holid" },
+//   { id: 1, name: "Holid" },
+//   { id: 1, name: "Holid" },
+//   { id: 1, name: "Holid" },
+//   { id: 1, name: "Holid" },
+//   { id: 1, name: "Holid" },
+//   { id: 1, name: "Holid" },
+//   { id: 1, name: "Holid" },
+// ];
+// let user1 = [
+//   { id: 1, name: "Holid" },
+//   { id: 1, name: "Holid" },
+//   { id: 1, name: "Holid" },
+//   { id: 1, name: "Holid" },
+//   { id: 1, name: "Holid" },
+//   { id: 1, name: "Holid" },
+//   { id: 1, name: "Holid" },
+//   { id: 1, name: "Holid" },
+// ];
+// function test() {
+//   console.log("hi ");
+// }
+// export { user, user1 };
+// export default test;
+// console.log(user);
+// console.log(user1);
+// test();
