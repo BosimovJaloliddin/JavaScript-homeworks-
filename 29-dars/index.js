b4a7175d056217426578a6377e50b23bee43e119
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-04-10 13:16:37 +0500

    Curry function

--- 29-dars/index.js
+++ 29-dars/index.js
@@ -3,3 +3,14 @@
 //   return (a) => (c) => console.log(a + b + c);
 // }
 // test(1)(2)(3);
+
+// ===========================
+// const test = (n) => {
+//   return (b) => {
+//     if (b) {
+//       return test(n + b);
+//     } else return n;
+//   };
+// };
+
+// console.log(test("hi")(" my")(" bro")());
