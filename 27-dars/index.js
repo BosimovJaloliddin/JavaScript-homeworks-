6886f105ee87c34a6159ee5dc19732c613de92ea
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-04-10 05:08:46 +0500

    Genrator function

---
+++ 27-dars/index.js
@@ -0,0 +1,17 @@
+// Generator function
+// function* test() {
+//   count = 0;
+//   while (1) {
+//     yield ++count;
+//     if (count === 100) return;
+//   }
+// }0
+// let gn = test();
+// console.log(gn.next().done);
+
+// for (i of gn) {
+//   console.log(i);
+// }
+// console.log(gn.next());
+// console.log(gn.next());
+// console.log(gn.next());
