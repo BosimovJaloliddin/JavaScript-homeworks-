2b3f5c17f46e74c7513d66a1c8c261a2a8c825d1
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-04-12 09:30:23 +0500

    DOM-2

---
+++ 34-dars/index.js
@@ -0,0 +1,44 @@
+let con = document.querySelector(".container");
+
+console.log(con.getBoundingClientRect());
+
+// function btn() {
+//   con.scrollTo(0, 100);
+// }
+
+// console.log(con.clientHeight);
+// console.log(con.offsetHeight);
+// console.log(con.clientWidth);
+// console.log(con.offsetWidth);
+// console.log(con.clientTop);
+// console.log(con.clientLeft);
+// console.log(con.offsetTop);
+// console.log(con.offsetLeft);
+// console.log(con.scrollHeight);
+// console.log(con.scrollTop);
+
+// function btn() {
+//   con.classList.remove("container");
+// }
+// style
+// con.style.cssText = `
+//     background-color:red;
+//     color:white;
+// `;
+
+// Clone Node
+// let box = document.querySelector(".box");
+// box = con.cloneNode(true);
+// console.log(box);
+
+// Creating element
+// let box = document.createElement("div");
+// let text = document.createTextNode("qwertyuiop");
+// box.prepend(text);
+// con.prepend(box);
+
+// Attribute
+// console.log(con.removeAttribute("class"));
+// console.log(con.setAttribute("id", "id"));
+// console.log(con.getAttribute("class"));
+// console.log(con.hasAttribute("id"));
