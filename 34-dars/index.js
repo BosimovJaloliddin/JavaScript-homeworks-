a49598df743556a604e52cf37e7fab7aee8e8ef8
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-04-12 12:28:33 +0500

    DOM-2

--- 34-dars/index.js
+++ 34-dars/index.js
@@ -1,6 +1,6 @@
-let con = document.querySelector(".container");
+// let con = document.querySelector(".container");
 
-console.log(con.getBoundingClientRect());
+// console.log(con.getBoundingClientRect());
 
 // function btn() {
 //   con.scrollTo(0, 100);
@@ -42,3 +42,48 @@ console.log(con.getBoundingClientRect());
 // console.log(con.setAttribute("id", "id"));
 // console.log(con.getAttribute("class"));
 // console.log(con.hasAttribute("id"));
+
+// =================================
+// homework
+let con = document.querySelector(".container");
+let conButton = document.querySelector(".button1");
+let button = document.querySelectorAll("button");
+
+con.style.cssText = `
+    font-size:24px;
+    font-weight:400;
+    width:400px;
+    height:400px;
+    padding:10px;
+    margin:0 auto;
+    overflow:auto;
+    border:2px solid aqua;
+`;
+
+conButton.style.cssText = `
+    display:flex;
+    justify-content: center;
+    gap:20px;
+    margin-top:20px;
+`;
+
+button.forEach((v) => {
+  v.style.cssText = `
+      font-size:24px;
+      font-weight:400;
+      width:200px;
+      height:40px;
+      background-color:blue;
+      color:white;
+      text-transle;
+      text-transform:uppercase;
+      border:none;
+  `;
+});
+
+function btnBottom() {
+  return con.scrollBy(0, 40);
+}
+function btnTop() {
+  return con.scrollBy(0, 340 - con.clientHeight);
+}
