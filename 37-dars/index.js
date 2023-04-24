e2e8d1ab20bb3271dfd5429d18bac651ebe34dd5
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-04-24 13:20:37 +0500

    Dom-5, UI & Events

---
+++ 37-dars/index.js
@@ -0,0 +1,26 @@
+// text.oncontextmenu = () => {
+//   alert("hellow world");
+// };
+// text.oncopy = () => {
+//   return false;
+// };
+
+// function onKeypress(e) {
+//   text.innerHTML = e.target.value;
+//   console.log(e.target.value);
+// }
+
+// =============
+// let box = document.querySelector(".box");
+// box.onmousemove = (e) => {
+//   //   box.style.backgroundColor = `rgb(${e.y},0,0)`;
+//   //   if (e.ctrlKey) box.style.backgroundColor = `rgb(${e.y},0,0)`;
+// };
+
+//==============
+// let box = document.querySelector(".box");
+// box.addEventListener("scroll", (e) => {
+//   if (e.target.scrollTop + e.target.clientHeight + 50 > e.target.scrollHeight) {
+//     box.textContent += box.textContent;
+//   }
+// });
