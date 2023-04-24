ef80890e352db05a37f89b967ae748e451c068e3
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-04-24 15:53:08 +0500

    form

--- 38-dars/index.js
+++ 38-dars/index.js
@@ -1,1 +1,23 @@
+// let form = document.forms.forOne;
+// let form = document.forms[0];
+// form.select.selectedIndex = 1;
+// console.log((form.select.value = "rus"));
 
+// onblur ,onfocus
+// let form = document.forms.forOne.inpOne;
+// form.onblur = (e) => {
+//   if (e.target.value.includes("@")) {
+//     errorH.textContent = null;
+//   } else {
+//     errorH.textContent = "No => @";
+//     errorH.style.color = "red";
+//   }
+// };
+// console.log(form);
+
+// onsubmit
+// let form = document.forms.forOne;
+// form.onsubmit = (e) => {
+//   form.action = `htt`;
+//   console.log(e.form.inpOne.value);
+// };
