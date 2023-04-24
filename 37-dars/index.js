ef9e6df80acb93a01faf0256a4aa04cce7a77edc
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-04-24 14:30:50 +0500

    Dom-5 UI & Events

--- 37-dars/index.js
+++ 37-dars/index.js
@@ -24,3 +24,68 @@
 //     box.textContent += box.textContent;
 //   }
 // });
+
+// oncontextmenu
+
+// text.addEventListener("contextmenu", () => {
+//   alert("Hellow");
+// });
+
+// oncopy
+// text.oncopy = () => false;
+
+// onkeypress
+// text.onkeypress = (e) => {
+//   head1.textContent = e.target.value;
+//   console.log(e.target.value);
+// };
+
+// onmousemove
+// let box = document.querySelector(".box");
+// box.onmousemove = (e) => {
+//   box.style.backgroundColor = `rgb(0,${e.y},0)`;
+//   console.log(e.y);
+// };
+
+// ondblclick
+// let box = document.querySelector(".box");
+// box.ondblclick = () => {
+//   box.style.backgroundColor = "red";
+// };
+
+// keyboard
+// ctrlKey=> Ctrl
+// shiftKey=>shift
+// altKey => alt (mac => opt)
+// metaKey=>window (mac=>cmd)
+// let box = document.querySelector(".box");
+// box.onmousemove = (e) => {
+//   if (e.metaKey) box.style.backgroundColor = `rgb(0,${e.y},0)`;
+// };
+
+// ===============
+// Keyboard actions
+// e.key
+// e.code
+// e.type
+
+// text.onkeypress = (e) => {
+//   console.log(e.key, e.code, e.type);
+// };
+
+// Scroll
+
+// let box = document.querySelector(".box");
+
+// box.addEventListener("scroll", (e) => {
+//   if (e.target.scrollTop + e.target.clientHeight + 50 > e.target.scrollHeight) {
+//     box.textContent += box.textContent;
+//   }
+// });
+
+// ===================
+// window.onscroll = (e) => {
+//   if (window.scrollY >= 20) {
+//     nav.style.boxShadow = "20px 10px 20px red";
+//   } else nav.style.boxShadow = null;
+// };
