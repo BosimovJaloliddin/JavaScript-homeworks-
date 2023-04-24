866f946cf085baf31c1ff1b29cc1a622a127a49e
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-04-24 11:59:20 +0500

    DOM-4

--- 36-dars/index.js
+++ 36-dars/index.js
@@ -1,17 +1,59 @@
+// function onClick() {
+//   console.log("onclick");
+// }
+// function onMousedown() {
+//   console.log("onMousedown");
+// }
+// function onMouseup() {
+//   console.log("onMouseup");
+// }
+
+// btn.addEventListener("click", () => onClick());
+// btn.addEventListener("mousedown", onMousedown);
+// btn.addEventListener("mouseup", onMouseup);
+
+// function onClick(event) {
+//   console.log("onclick");
+// }
+// function onMousedown() {
+//   console.log("onMousedown");
+// }
+// function onMouseup() {
+//   console.log("onMouseup");
+// }
 // class Events {
-//   handleEvent(e) {
-//     console.log(e);
+//   handleEvent(event) {
+//     switch (event.type) {
+//       case "click":
+//         onClick();
+//         break;
+//       case "mousedown":
+//         onMousedown();
+//         break;
+//       case "mouseup":
+//         onMouseup();
+//         break;
+//     }
 //   }
 // }
 // let evn = new Events();
-
 // btn.addEventListener("click", evn);
 // btn.addEventListener("mousedown", evn);
 // btn.addEventListener("mouseup", evn);
-`
-let target;
-tb.onclick = function (e) {
-  if (target) target.classList.remove("adding");
-  target = e.target;
-  target.classList.add("adding");
-};`;
+
+// function grand() {
+//   console.log("grand");
+// }
+// function parent(e) {
+//   // e.stopImmediatePropagation();
+//   e.stopPropogation();
+//   console.log("parent");
+// }
+
+// function child() {
+//   // e.stopPropagation();
+//   console.log("child");
+// }
+
+// ========
+// link.onc0
