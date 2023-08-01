cb7ee7ec2bcf95b8fdd62fcece8988ff3e95b3df
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-08-01 21:52:33 +0500

    add Fetch-2

--- 47-dars/index.js
+++ 47-dars/index.js
@@ -3,19 +3,41 @@
 //   .then((res) => {
 //     console.log(res);
 //   });
-
-formEvent.onsubmit = (e) => {
-  e.preventDefault();
-  let body = new FormData(formEvent);
-  body.append("userImg", file.files[0]);
-  body.append("secretNum", "12");
-  body.delete("email");
-  fetch("http://httpbin.org/post", {
+const login = async () => {
+  fetch("http://localhost:8081/api/public/auth/login", {
     method: "POST",
-    body,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email.value,
+      password: password.value,
+    }),
   })
     .then((res) => res.json())
     .then((res) => {
       console.log(res);
+      localStorage.setItem("token", res?.authenticationToken);
     });
+};
+
+const onSubmit = async () => {
+  let res = await fetch(`http://localhost:8081/api/v1/categories/list`, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  }).then((res) => res.json());
+  console.log(res);
+};
+
+const getCategory = async () => {
+  let res = await fetch("http://localhost:8081/api/v1/houses/list").then(
+    (res) => res.json()
+  );
+
+  res.data?.forEach((v) => {
+    div = document.createElement("div");
+    div.innerHTML = `${v.id} - ${v.name}  <button onclick="onSubmit()">get info</button>`;
+    category.append(div);
+  });
 };
