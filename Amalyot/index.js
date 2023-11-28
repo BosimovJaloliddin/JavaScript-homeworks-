6c9e77bc6f73a96c153be9358ec7200a8c0c8f58
Jaloliddin Bosimov <bosimovjaloliddin@gmail.com>
2023-11-28 10:16:39 +0500

    variable

--- Amalyot/index.js
+++ Amalyot/index.js
@@ -3006,3 +3006,17 @@
 // let str = "hellow";
 // let str = "world";
 // console.log(str);
+// ======================================
+console.log(
+  (function func() {
+    var name = "ali";
+    return name;
+  })()
+);
+
+console.log(
+  (function func() {
+    var name = "vali";
+    return name;
+  })()
+);
