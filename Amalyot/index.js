b7542f3ce5fa4cb2c99330b7c3d93e622216c992
Jaloliddin Bosimov <bosimovjaloliddin@gmail.com>
2024-06-17 19:05:16 +0500

    add class

--- Amalyot/index.js
+++ Amalyot/index.js
@@ -3568,3 +3568,96 @@
 
 // let cars = new Cars("Cobalt", 200);
 // cars.getInfo();
+
+// function User() {
+//   this.name = "Ali";
+//   this.age = 23;
+// }
+// let dataUser = new User();
+
+// console.log(dataUser);
+
+// class Cl {
+//   constructor() {
+//     this.name = "Ali";
+//     this.age = 23;
+//   }
+// }
+// let cl = new Cl();
+
+// console.log(cl);
+
+//=========================
+// function Clock({ template }) {
+//   let timer;
+
+//   function render() {
+//     let date = new Date();
+
+//     let hours = date.getHours();
+//     if (hours < 10) hours = "0" + hours;
+
+//     let mins = date.getMinutes();
+//     if (mins < 10) mins = "0" + mins;
+
+//     let secs = date.getSeconds();
+//     if (secs < 10) secs = "0" + secs;
+
+//     let output = template
+//       .replace("h", hours)
+//       .replace("m", mins)
+//       .replace("s", secs);
+
+//     console.log(output);
+//   }
+
+//   this.stop = function () {
+//     clearInterval(timer);
+//   };
+
+//   this.start = function () {
+//     render();
+//     timer = setInterval(render, 1000);
+//   };
+// }
+
+// let clock = new Clock({ template: "h:m:s" });
+// // clock.start();
+// // console.log(clock);
+
+// class ClassCLock {
+//   //   constructor({ template }) {
+//   //     this.template = template;
+//   //   }
+//   timer;
+//   render = function () {
+//     let template = "h:m:s";
+//     let date = new Date();
+
+//     let hours = date.getHours();
+//     if (hours < 10) hours = "0" + hours;
+
+//     let mins = date.getMinutes();
+//     if (mins < 10) mins = "0" + mins;
+
+//     let secs = date.getSeconds();
+//     if (secs < 10) secs = "0" + secs;
+
+//     let output = template
+//       .replace("h", hours)
+//       .replace("m", mins)
+//       .replace("s", secs);
+
+//     console.log(output);
+//     // console.log(hours);
+//     // console.log(template);
+//   };
+
+//   start() {
+//     this.render();
+//     this.timer = setInterval(this.render, 1000);
+//   }
+// }
+// let classClock = new ClassCLock();
+// classClock.start();
+// // console.log(classClock);
